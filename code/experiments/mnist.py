0f7317b9c380cc60ef315d5cd62262bdb3cfcef7
Lucas Theis <lucas@theis.io>
2015-01-27 17:33:37 +0100

    Minor changes.

--- code/experiments/mnist.py
+++ code/experiments/mnist.py
@@ -17,11 +17,16 @@ from trmix import MoBernoulli
 def main(argv):
 	parser = ArgumentParser(argv[0], description=__doc__)
 	parser.add_argument('--num_components', '-c', type=int, default=20)
-	parser.add_argument('--max_epochs',     '-e', type=int, default=4)
+	parser.add_argument('--max_epochs',     '-E', type=int, default=4)
 	parser.add_argument('--max_iter_tr',    '-m', type=int, default=5,
 		help='Number of steps in the inner loop of the trust-region method.')
+	parser.add_argument('--output',         '-o', type=str, default='results/mnist/')
 
 	args = parser.parse_args(argv[1:])
+
+	# create directories if necessary
+	if not os.path.exists(args.output):
+		os.makedirs(args.output)
 
 	experiment = Experiment()
 
@@ -37,12 +42,14 @@ def main(argv):
 		callback.num_updates.append(model.num_updates)
 		callback.lower_bound.append(model.lower_bound(data))
 
+		print callback.lower_bound[-1]
+
 		p = []
 		for k in range(len(model)):
 			p.append(model[k].alpha / (model[k].alpha + model[k].beta))
 		p = hstack(p)
 
-		imsave('results/mnist/mnist.{0}.png'.format(callback.counter),
+		imsave(os.path.join(args.output, 'mnist.{0}.png').format(callback.counter),
 			stitch(p.T.reshape(-1, 28, 28), num_rows=4), cmap='gray', vmin=0., vmax=1.)
 
 		callback.counter += 1
@@ -51,7 +58,7 @@ def main(argv):
 	callback.num_updates = []
 	callback.lower_bound = []
 
-	os.system('rm -f results/mnist/mnist.*.png')
+	os.system('rm -f {0}'.format(os.path.join(args.output, 'mnist.*.png')))
 
 	try:
 		model = MoBernoulli(dim=784, num_components=args.num_components)
@@ -68,11 +75,14 @@ def main(argv):
 	experiment['model'] = model
 	experiment['num_updates'] = callback.num_updates
 	experiment['lower_bound'] = callback.lower_bound
-	experient.save('results/mnist/mnist.{0}.{1}.{{0}}.{{1}}.xpck'.format(
+	experiment.save(os.path.join(args.output, 'mnist.{0}.{1}.{{0}}.{{1}}.xpck').format(
 		args.num_components, args.max_iter_tr))
 
- 	os.system('ffmpeg -r 25 -i results/mnist/mnist.%d.png -vcodec mjpeg -qscale 0 mnist.{0}.{1}.avi'.format(
-		args.num_components, args.max_iter_tr))
+ 	os.system('ffmpeg -r 25 -i {1} -vcodec mjpeg -sameq {0}'.format(
+		os.path.join(
+			args.output,
+			'mnist.{0}.{1}.avi'.format(args.num_components, args.max_iter_tr)),
+		os.path.join(args.output, 'mnist.%d.png')))
 
 	return 0
 
